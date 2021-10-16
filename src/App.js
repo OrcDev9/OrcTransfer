0702671667ea7f54084feb94c01daba6984993a9
OrcDev9 <92228005+OrcDev9@users.noreply.github.com>
2021-10-16 09:51:26 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -88,7 +88,7 @@ function App() {
                         
                         {!!tokenIds && tokenIds.length > 0 && showUnstake &&
                             <div className="mt-4">
-                                <button onClick={() => unstake()}>Unstake</button>
+                                <button onClick={() => unstake()}>Unstake (Step 2)</button>
                             </div>
                         }
 
@@ -102,7 +102,7 @@ function App() {
                                 </select>
                             </div>
                             <div className="pl-4">
-                                <button onClick={() => transactContractWithWallet(contract, 'setApprovalForAll', approved)}>setApprovalForAll</button>
+                                <button onClick={() => transactContractWithWallet(contract, 'setApprovalForAll', approved)}>setApprovalForAll (Step 3)</button>
                             </div>
                         </div> */}
 
@@ -114,7 +114,7 @@ function App() {
                         </div> 
 
 
-                        <div className="mb-2 mt-10">Value (single int):</div>
+                        <div className="mb-2 mt-10">Migrate Single Orcs (single int):</div>
                         <div className="flex">
                             <div>
                             <input type="text" style={{width: 450}} value={singleint} onChange={e => setSingleint(e.target.value)}></input>
@@ -136,7 +136,7 @@ function App() {
                         </div>
 
 
-                        <div className="mb-2 mt-10">Values (comma separated):</div>
+                        <div className="mb-2 mt-10">Migrate Many Orcs (comma separated):</div>
                         <div className="flex mb-48">
                             <div>
                             <input type="text" style={{width: 450}} value={ints} onChange={e => setInts(e.target.value)}></input>
