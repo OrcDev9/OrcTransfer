640c12ba15f1c53d4d7ac8980188fe17c9f8f296
OrcDev9 <92228005+OrcDev9@users.noreply.github.com>
2021-10-16 17:03:13 -0700

    Update blockchain.js

--- src/lib/blockchain.js
+++ src/lib/blockchain.js
@@ -25,7 +25,7 @@ export async function getTokenIdsForAddress(contract){
     var tokenIds = [];
     let calls = [];
     let results = [];
-    for (let index = 1; index < 4234; index++) {
+    for (let index = 1; index < 4355; index++) {
         calls.push(orcContract.ownerOf(index));
         if (calls.length === 300) {
             let partial = await ethcallProvider.all(calls);
